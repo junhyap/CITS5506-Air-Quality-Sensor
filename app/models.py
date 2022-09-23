4b2fcff90be828f04ab002f452381363f0030a15
Jun <6574274+skyheat@users.noreply.github.com>
2022-09-23 14:02:40 +0800

    Revert "Api"

--- app/models.py
+++ app/models.py
@@ -1,34 +1,8 @@
-from datetime import datetime
-from flask import url_for
 from app import db
+from datetime import datetime
 
 
-class PaginatedAPIMixin(object):
-    @staticmethod
-    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
-        resources = query.paginate(page, per_page, False)
-        data = {
-            "items": [item.to_dict() for item in resources.items],
-            "_meta": {
-                "page": page,
-                "per_page": per_page,
-                "total_pages": resources.pages,
-                "total_items": resources.total,
-            },
-            "_links": {
-                "self": url_for(endpoint, page=page, per_page=per_page, **kwargs),
-                "next": url_for(endpoint, page=page + 1, per_page=per_page, **kwargs)
-                if resources.has_next
-                else None,
-                "prev": url_for(endpoint, page=page - 1, per_page=per_page, **kwargs)
-                if resources.has_prev
-                else None,
-            },
-        }
-        return data
-
-
-class AirQuality(PaginatedAPIMixin, db.Model):
+class AirQuality(db.Model):
     # id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(
         db.String(64),
@@ -45,20 +19,3 @@ class AirQuality(PaginatedAPIMixin, db.Model):
 
     def __repr__(self):
         return "<Air Quality {}>".format(self.string)
-
-    def to_dict(self):
-        data = {
-            "timestamp": self.timestamp,
-            "temp": self.temp,
-            "humidity": self.humidity,
-            "particles": self.particles,
-            "eco2": self.eco2,
-            "tvoc": self.tvoc,
-        }
-
-        return data
-
-    def from_dict(self, data):
-        for field in ["temp", "humidity", "particles", "eco2", "tvoc"]:
-            if field in data:
-                setattr(self, field, data[field])
